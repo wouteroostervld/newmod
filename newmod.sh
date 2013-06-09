c62d9e6e37e41a801f3b2a4bc7fae49f09110fb2
Wouter Oosterveld <wouter@fizzyflux.nl>
2013-06-09 22:26:26 +0200

    initial commit

---
+++ newmod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]; then
+    usage
+    exit 1
+fi
+
+if [ -e "$1" ]; then
+    echo 2>&1 "$1" already exists.
+    exit 1
+fi
+
+cp -a skeleton "$1"
+mv "$1"/NAME "$1"/"$1"
+mv "$1"/tests/NAME_tests.py "$1"/tests/"$1"_tests.py
+sed -i -e s/'My Project'/"$1"/ -e s/NAME/"$1"/ -e s/projectname/"$1"/ "$1"/setup.py
+
