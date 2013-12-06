8a80daae126dabc71bc7bb765990b9fc89d0b4fb
Wouter Oosterveld <wouter@fizzyflux.nl>
2013-12-06 15:14:09 +0100

    Fix sed on freebsd. Prob break it on Linux.

--- newmod.sh
+++ newmod.sh
@@ -15,5 +15,5 @@ fi
 cp -a skeleton "$1"
 mv "$1"/NAME "$1"/"$1"
 mv "$1"/tests/NAME_tests.py "$1"/tests/"$1"_tests.py
-sed -i -e s/'My Project'/"$1"/ -e s/NAME/"$1"/ -e s/projectname/"$1"/ "$1"/setup.py
+sed -i '' -e s/'My Project'/"$1"/ -e s/NAME/"$1"/ -e s/projectname/"$1"/ "$1"/setup.py
 
